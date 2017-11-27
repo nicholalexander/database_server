e8eb1bea3c5ef7605cb0e9b9d1d905a6dde23ad2
nichol alexander <nichol.alexander@gmail.com>
2017-11-27 12:38:31 -0500

    persisting in database class

--- database.rb
+++ database.rb
@@ -5,9 +5,23 @@ require 'pry'
 # sets value with http://localhost:4000/set?somekey=somevalue 
 # gets value with http://localhost:4000/get?key=somekey
 
-@@data = {}
+class Database
+  attr_accessor :data_hash
+  def initialize
+    @data_hash = {}
+  end
+end
+
 
 class DatabaseServer < WEBrick::HTTPServlet::AbstractServlet
+  
+  attr_accessor :database
+
+  def initialize(server, database)
+    super server
+    @database = database
+  end
+
   def do_GET (request, response)
 
     response.status = 200
@@ -16,10 +30,12 @@ class DatabaseServer < WEBrick::HTTPServlet::AbstractServlet
 
     case request.path
     when "/get"
-      result = @@data[request.query['key']]
+      result = @database.data_hash[request.query['key']]
     when "/set"
-      @@data.merge! request.query
-      result = @@data
+      @database.data_hash.merge! request.query
+      result = @database.data_hash
+    when "/show"
+      result = @database.data_hash
     else
       result = "No such method"
     end
@@ -27,11 +43,14 @@ class DatabaseServer < WEBrick::HTTPServlet::AbstractServlet
     response.body = result.to_s + "\n"
   
   end
+
 end
+
+database = Database.new
 
 server = WEBrick::HTTPServer.new(:Port => 4000)
 
-server.mount "/", DatabaseServer
+server.mount "/", DatabaseServer, database
 
 trap("INT") {
   server.shutdown
