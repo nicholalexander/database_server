3934055dc904b32565080a8514668e5e195c6bef
nichol alexander <nichol.alexander@gmail.com>
2017-12-06 13:00:18 -0500

    playing with writing data.

--- database.rb
+++ database.rb
@@ -1,14 +1,24 @@
 require 'webrick'
+require 'yaml'
 
-# runs on http://localhost:4000/. 
-# sets value with http://localhost:4000/set?somekey=somevalue 
+# runs on http://localhost:4000/.
+# sets value with http://localhost:4000/set?somekey=somevalue
 # gets value with http://localhost:4000/get?key=somekey
+
+# handle missing data.yaml
+# multiple key/value pairs and more error handling on inputs
+# yaml store or pstore under multiple clients
+
+class DatabasePersistence
+end
 
 class Database
   attr_reader :data_hash
-  
+
   def initialize
     @data_hash = Hash.new(nil)
+    @file_name = './data.yaml'
+    read_from_disk
   end
 
   def get(key)
@@ -17,13 +27,24 @@ class Database
 
   def set(key_value_pair)
     @data_hash.merge! key_value_pair
+    write_to_disk
     return key_value_pair
+  end
+
+  private
+
+  def write_to_disk
+    File.open("#{@file_name}", 'w') {|f| f.write @data_hash.to_yaml } #Store
+  end
+
+  def read_from_disk
+    @data_hash = YAML::load_file("#{@file_name}")
   end
 end
 
 class DatabaseServer < WEBrick::HTTPServlet::AbstractServlet
   attr_accessor :database
-  
+
   def initialize(server, database)
     super server
     @database = database
